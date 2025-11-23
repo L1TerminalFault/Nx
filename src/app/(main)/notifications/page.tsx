d2d157d7c3717a4a525c845cf3cf7b7d5fec945a
Error <null>
2025-11-23 19:20:17 +0300

    done with the socket client handler

--- src/app/(main)/notifications/page.tsx
+++ src/app/(main)/notifications/page.tsx
@@ -1,43 +1,60 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
+
+type Notification = {
+  _id: string;
+  userId: string;
+  message: string;
+  time: string;
+};
 
 export default function Notification() {
-  const [notifications, setNotifications] = useState([]);
+  const [notifications, setNotifications] = useState<Notification[]>([]);
   const [loading, setLoading] = useState(true);
   const PORT = 30000;
 
   useEffect(() => {
-    const socket = io(`${window.location.origin}:${PORT}`);
+    let socket: Socket;
 
-    socket.on("message", (notif) =>
-      setNotifications((prev) => [...prev, notif]),
-    );
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  useEffect(() => {
     (async () => {
       setLoading(true);
       const messagesFetched = await (
         await fetch("/api/getNotifications")
       ).json();
       setNotifications(messagesFetched);
+
+      socket = io(`${window.location.origin}:${PORT}`);
+      socket.on("message", (notif: Notification) =>
+        setNotifications((prev: Notification[]): Notification[] => [
+          ...prev,
+          notif,
+        ]),
+      );
+
       setLoading(false);
     })();
+
+    return () => {
+      socket?.disconnect();
+    };
   }, []);
 
   return (
     <div>
       <div className="text-xl">Notifications Page</div>
-      {loading
-        ? null
-        : notifications.map((notification) => (
-            <div key={null}>{notification.message}</div>
-          ))}
+      {loading ? (
+        <div>Loading</div>
+      ) : !notifications ? (
+        <div>Could not load notifications</div>
+      ) : !notifications?.length ? (
+        <div>No notifications</div>
+      ) : (
+        notifications?.map((notification) => (
+          <div key={null}>{notification.message}</div>
+        ))
+      )}
     </div>
   );
 }
