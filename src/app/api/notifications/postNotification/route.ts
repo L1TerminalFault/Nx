c062a88ce8960c3a78fa2b7cf471be691c17ec63
Error <null>
2025-11-25 17:16:17 +0300

    route postnotifications checked

--- src/app/api/notifications/postNotification/route.ts
+++ src/app/api/notifications/postNotification/route.ts
@@ -3,13 +3,14 @@ import { Message } from "@/lib/db";
 import { getIO } from "@/lib/socket";
 
 export async function POST(request: Request) {
-  const message = await request.json();
+  const message: { userId: string; message: string; time: string } =
+    await request.json();
 
   console.log("Message Recieved: ", message);
   await Message.create(message);
 
   const io = getIO();
-  io.emit("message", message);
+  io.emit("message", { ...message, _id: 1 });
 
   return Response.json({ status: "success" });
 }
