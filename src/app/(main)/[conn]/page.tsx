b38cb442d98989f7c9642408daff152f19b30bc0
xDk <linuxkali717@gmail.com>
2026-02-05 21:50:33 +0300

    some ui fixes

--- src/app/(main)/[conn]/page.tsx
+++ src/app/(main)/[conn]/page.tsx
@@ -123,7 +123,7 @@ export default function Notification() {
   }, [connectionString, router, lastSegment]);
 
   return (
-    <div className="min-h-screen min-w-screen bg-gray-900/10">
+    <div className="min-h-screen bg-gray-900/10">
       <div className="w-full scrollbar-hidden overflow-scroll h-full py-16 flex flex-col items-center p-3 text-white transition-all">
         <div className="w-full flex items-center justify-between fixed top-0 p-4 text-xl border-transparent transition-all border-b-gray-700/30 border backdrop-blur-xl bg-transparent">
           <div className="flex items-center gap-2">
@@ -234,7 +234,7 @@ export default function Notification() {
                 No notifications
               </div>
             ) : (
-              <div className="w-full h-full flex flex-col gap-4 py-2">
+              <div className="w-full h-full flex flex-col gap-4 py-2 scrollbar-hidden">
                 {notifications?.map((notification) => (
                   <div
                     key={notification._id}
