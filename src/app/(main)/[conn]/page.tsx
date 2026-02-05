a304644e43dd9983ca6010f0edcc50c7f45c4be2
xDk <linuxkali717@gmail.com>
2026-02-05 18:20:37 +0300

    some ui fixes

--- src/app/(main)/[conn]/page.tsx
+++ src/app/(main)/[conn]/page.tsx
@@ -74,7 +74,7 @@ export default function Notification() {
   );
 
   return (
-    <div className="w-screen relative h-screen flex flex-col items-center pt-20 pb-10 p-5 bg-gray-900/20 text-white transition-all">
+    <div className="w-screen relative scrollbar-hidden overflow-scroll h-screen flex flex-col items-center pt-20 pb-10 p-5 bg-gray-900/20 text-white transition-all">
       <div className="w-full flex items-center justify-between fixed top-0 p-4 text-xl border-transparent transition-all border-b-gray-700/30 border backdrop-blur-xl bg-transparent">
         <div className="flex items-center gap-2">
           <div>
@@ -156,7 +156,7 @@ export default function Notification() {
           </div>
         </div>
       ) : (
-        <div className="w-full h-full py-10 scrollbar-hidden overflow-scroll flex items-center justify-center">
+        <div className="w-full h-full py-10scrollbar-hiddenoverflow-scroll flex items-center justify-center">
           {error ? (
             <div className="p-4 text-xl w-full flex justify-center">
               {error}
@@ -176,11 +176,11 @@ export default function Notification() {
                   key={notification._id}
                   className="flex justify-between gap-2 flex-row bg-[#ffffff03] p-1.5 rounded-2xl"
                 >
-                  <div className="flex flex-col gap-2">
+                  <div className="flex flex-col gap-1.5">
                     <div className="font-bold px-3 pt-1">
                       {notification.title}
                     </div>
-                    <div className="text-sm text-gray-300 border-gray-700/20 rounded-2xl bg-[#ffffff06] border p-2">
+                    <div className="text-sm text-gray-300 border-gray-700/20 rounded-2xl bg-[#ffffff06] border px-2.5 py-1.5">
                       {notification.message}
                     </div>
                   </div>
