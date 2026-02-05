30656af7cad799885c6ae016e3b55dd92e821f43
xDk <linuxkali717@gmail.com>
2026-02-05 16:49:38 +0300

    some ui fixes

--- src/app/(main)/[conn]/page.tsx
+++ src/app/(main)/[conn]/page.tsx
@@ -71,8 +71,8 @@ export default function Notification() {
   const [code, setCode] = useState<string>(generateCode());
 
   return (
-    <div className="w-screen relative h-screen flex flex-col items-center pt-18 p-5 bg-gray-900/20 text-white">
-      <div className="w-full absolute top-0  p-4 text-xl border-transparent border-b-gray-900 border backdrop-blur-2xl bg-white/5">
+    <div className="w-screen relative h-screen flex flex-col items-center pt-20 p-5 bg-gray-900/20 text-white">
+      <div className="w-full absolute top-0  p-4 text-xl border-transparent border-b-gray-900 border backdrop-blur-2xl bg-transparent">
         NxServer
       </div>
       <div
@@ -80,7 +80,7 @@ export default function Notification() {
           localStorage.removeItem("__nx_connection_string__");
           router.replace("/configure");
         }}
-        className="fixed bottom-6 right-6 py-2 px-5 flex z-50 justify-end bg-white/15 backdrop-blur-2xl shadow-lg shadow-black rounded-full cursor-pointer"
+        className="fixed bottom-6 right-6 py-2 px-5 flex z-50 justify-end bg-white/10 hover:bg-white/15 backdrop-blur-2xl shadow-lg shadow-black rounded-full cursor-pointer"
       >
         RECONFIGURE
       </div>
@@ -99,13 +99,13 @@ export default function Notification() {
           </div>
           <div className="flex flex-row gap-12 p-4 justify-around">
             <div
-              className="py-2 px-4 rounded-full bg-white/5"
+              className="py-2 px-4 rounded-full bg-white/5 hover:bg-white/10"
               onClick={() => setCode(generateCode())}
             >
               New Code
             </div>
             <div
-              className="py-2 px-4 rounded-full bg-white/5"
+              className="py-2 px-4 rounded-full bg-white/5 hover:bg-white/10"
               onClick={() => {
                 localStorage.setItem("__nx_connection_string__", code);
                 router.replace(`/${code}`);
