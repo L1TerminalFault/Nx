fa6ddecfd3e84bfa191e72d7fa32342cbb6e16f9
xDk <linuxkali717@gmail.com>
2026-02-05 18:14:05 +0300

    some ui fixes

--- src/app/(main)/[conn]/page.tsx
+++ src/app/(main)/[conn]/page.tsx
@@ -75,7 +75,7 @@ export default function Notification() {
 
   return (
     <div className="w-screen relative h-screen flex flex-col items-center pt-20 pb-10 p-5 bg-gray-900/20 text-white transition-all">
-      <div className="w-full h-full flex items-center justify-between fixed top-0 p-4 text-xl border-transparent transition-all border-b-gray-700/30 border backdrop-blur-xl bg-transparent">
+      <div className="w-full flex items-center justify-between fixed top-0 p-4 text-xl border-transparent transition-all border-b-gray-700/30 border backdrop-blur-xl bg-transparent">
         <div className="flex items-center gap-2">
           <div>
             NxServer <span className="text-sm text-gray-500">v1.0.0</span>
