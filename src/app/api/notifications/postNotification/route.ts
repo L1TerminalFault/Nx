380b39bf8a80802e75705b1d31979e81ba83cce7
xDk <linuxkali717@gmail.com>
2026-02-27 22:55:59 +0300

    set result limit to 15 on database side

--- src/app/api/notifications/postNotification/route.ts
+++ src/app/api/notifications/postNotification/route.ts
@@ -2,6 +2,8 @@ import { Message } from "@/lib/db";
 // import { NextResponse as Response } from "next/server";
 
 // import { getIO } from "@/lib/socket";
+//
+const MAX_DOCS = 15;
 
 type Message = {
   _id: string;
@@ -17,6 +19,19 @@ export async function POST(request: Request) {
   // const messageRtr: Message =
   try {
     await Message.create(message);
+
+    const entries_to_delete = await Message.find({
+      connectionString: message.connectionString,
+    })
+      .sort({ time: -1 })
+      .skip(MAX_DOCS)
+      .select("_id");
+
+    if (entries_to_delete.length > 0) {
+      const ids_to_delete = entries_to_delete.map((entry) => entry._id);
+      Message.deleteMany({ _id: { $in: ids_to_delete } }).exec();
+    }
+
     return Response.json({ status: "success" });
   } catch (error) {
     console.error("Error saving message: ", error);
